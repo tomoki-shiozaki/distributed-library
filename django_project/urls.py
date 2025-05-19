7409c9cf9585416ee80170a0b5bbd3cad945e370
shiozaki <shiozaki.tomoki2@gmail.com>
2025-05-19 14:37:14 +0900

    feat(home): copy home app and templates

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("apps.home.urls")),
 ]
