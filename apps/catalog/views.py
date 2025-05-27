975cfa39115d645a5c58ca3faf9c2953e441d751
shiozaki <shiozaki.tomoki2@gmail.com>
2025-05-27 11:34:45 +0900

    feat(catalog): add book registration form without retrieving book data from API

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
+from apps.catalog.models import Book
 
 
 # Create your views here.
-class BookCreateView(TemplateView):
+class BookCreateView(CreateView):
     template_name = "catalog/book_form.html"
+    model = Book
+    fields = (
+        "isbn",
+        "title",
+        "author",
+        "publisher",
+        "published_date",
+        "image_url",
+        "edition",
+    )
