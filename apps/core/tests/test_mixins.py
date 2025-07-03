ce808f2b385a363ec4b6222f98e9949a6890db5a
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-03 11:34:37 +0900

    refactor(core): update tests for IsLibrarianMixin

--- apps/core/tests/test_mixins.py
+++ apps/core/tests/test_mixins.py
@@ -8,9 +8,14 @@ from apps.core.mixins import (
 User = get_user_model()
 
 
+class DummyRequest:
+    def __init__(self, user):
+        self.user = user
+
+
 class DummyLibrarianView(IsLibrarianMixin):
     def __init__(self, user):
-        self.request = type("Request", (), {"user": user})()
+        self.request = DummyRequest(user)
 
 
 class IsLibrarianMixinUnitTest(TestCase):
@@ -24,8 +29,8 @@ class IsLibrarianMixinUnitTest(TestCase):
 
     def test_librarian_passes_test_func(self):
         view = DummyLibrarianView(self.librarian)
-        assert view.test_func() is True
+        self.assertTrue(view.test_func())
 
     def test_general_user_fails_test_func(self):
         view = DummyLibrarianView(self.general)
-        assert view.test_func() is False
+        self.assertFalse(view.test_func())
