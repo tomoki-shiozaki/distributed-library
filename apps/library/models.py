8176a0d9e7c4e32f454a63b825432964051ee7f9
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-14 12:01:53 +0900

    feat(library): update ReservationHistory model

--- apps/library/models.py
+++ apps/library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from apps.catalog.models import Book, Copy
 
@@ -39,7 +40,7 @@ class ReservationHistory(models.Model):
         COMPLETED = "completed", "完了"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="利用者")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="本")
+    copy = models.ForeignKey(Copy, on_delete=models.CASCADE, verbose_name="蔵書")
     start_date = models.DateField(verbose_name="予約開始日")
     end_date = models.DateField(verbose_name="予約終了日")
     status = models.CharField(
@@ -50,9 +51,23 @@ class ReservationHistory(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="予約作成日")
 
+    def clean(self):
+        overlapping = ReservationHistory.objects.filter(
+            copy=self.copy,
+            status=ReservationHistory.Status.RESERVED,
+            start_date__lt=self.end_date,
+            end_date__gt=self.start_date,
+        ).exclude(id=self.id)
+        if overlapping.exists():
+            raise ValidationError("この蔵書には、重複する予約がすでに存在します。")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     class Meta:
         verbose_name = "予約履歴"
         verbose_name_plural = "予約履歴"
 
     def __str__(self):
-        return f"{self.user} - {self.book} ({self.status})"
+        return f"{self.user} - {self.copy} ({self.status})"
