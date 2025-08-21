54b82837f11d54644611d36e309257bdcb2592e0
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-21 18:30:50 +0900

    chore(settings): restrict ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 # Application definition
