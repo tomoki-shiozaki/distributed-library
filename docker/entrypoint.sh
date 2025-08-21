19ff41c3f674feb5172c1d6cfb927f49f78a15ca
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-21 19:00:35 +0900

    chore: implement collectstatic in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,10 @@ export DJANGO_SETTINGS_MODULE=django_project.settings
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+# 静的ファイル収集
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 # アプリ起動
 echo "Starting Gunicorn..."
 exec gunicorn django_project.wsgi:application --bind 0.0.0.0:$PORT --workers 3
